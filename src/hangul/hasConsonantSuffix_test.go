8239468d7f661ad26ffd6549bb8361b95d10bb91
zhunhe <xuzhunhe@gmail.com>
2021-05-19 04:14:46 +0900

    테스트케이스 구조체명 변경

--- src/hangul/hasConsonantSuffix_test.go
+++ src/hangul/hasConsonantSuffix_test.go
@@ -2,18 +2,18 @@ package hangul
 
 import "testing"
 
-type consonantSuffixTest struct {
+type testCase struct {
 	in  string
 	out bool
 }
 
-var consonantSuffixTests = []consonantSuffixTest{
-	consonantSuffixTest{"가", false},
-	consonantSuffixTest{"각", true},
+var testCases = []testCase{
+	testCase{"가", false},
+	testCase{"각", true},
 }
 
 func TestHasConsonantSuffix(t *testing.T) {
-	for _, e := range consonantSuffixTests {
+	for _, e := range testCases {
 		got := HasConsonantSuffix(e.in)
 		if got != e.out {
 			t.Errorf("got: [%v], but want: [%v]", got, e.out)
