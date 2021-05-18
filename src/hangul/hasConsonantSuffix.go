b6efef505aabf4254450208af6644df739a3c697
zhunhe <xuzhunhe@gmail.com>
2021-05-18 09:29:22 +0900

    Modify hasConsonantSuffix.go 함수설명 주석 추가

--- src/hangul/hasConsonantSuffix.go
+++ src/hangul/hasConsonantSuffix.go
@@ -1,10 +1,13 @@
 package hangul
 
 var (
-	start = '가'
-	end   = '힣'
+	start = '가' // rune(44032)
+	end   = '힣' // rune(55203)
 )
 
+// 받침이 있으면 true, 없으면 false를 return하는 프로그램
+// "아무개이(가) 찾아왔습니다."와 같이 매끄럽지 못한 문장이 아니라
+// "아무개가 찾아왔습니다", "한글이 찾아왔습니다"와 같은 문장을 사용할 수 있도록..
 func HasConsonantSuffix(s string) bool {
 	numEnds := 28
 	result := false
