2f28941fa6ec6ccd69c248f921e9582454b5cd73
zhunhe <xuzhunhe@gmail.com>
2021-05-27 12:35:24 +0000

    여러 Case 추가

--- src/CHAP_03/strCat/strCat.go
+++ src/CHAP_03/strCat/strCat.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
-func strCat() {
-	s := "abc"
-	ps := &s
-	s += "def"
-	fmt.Println(s)   // result [abcdef]
-	fmt.Println(*ps) // result [abcdef]
+func strCat(s1, s2 string) string {
+	return s1 + s2
 }
 
 func main() {
-	strCat()
+	s1, s3, s5 := "abc", "abc", "abc"
+	s2, s4, s6 := "def", "def", "def"
+
+	// Case 1
+	s1 = strCat(s1, s2)
+	// Case 2
+	s3 = fmt.Sprint(s3, s4)
+	// Case 3
+	s5 = fmt.Sprintf("%s%s", s5, s6)
+
+	fmt.Printf("s1: %s, s3: %s, s5: %s\n", s1, s3, s5)
 }
