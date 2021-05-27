5a8e6c59e12760610ae04608b4f320a5cd4b2d45
zhunhe <xuzhunhe@gmail.com>
2021-05-27 12:56:45 +0000

    문자열을 숫자로 하는 다른 방법 추가

--- src/CHAP_03/strconv/strconv.go
+++ src/CHAP_03/strconv/strconv.go
@@ -11,21 +11,13 @@ func main() {
 	var f float64
 	var s string
 
-	i, _ = strconv.Atoi("350")
-	fmt.Println(i)
-
-	k, _ = strconv.ParseInt("cc7fdd", 16, 32)
-	fmt.Println(k)
-
-	k, _ = strconv.ParseInt("0xcc7fdd", 0, 32)
-	fmt.Println(k)
-
-	f, _ = strconv.ParseFloat("3.14", 64)
-	fmt.Println(f)
-
-	s = strconv.Itoa(340)
-	fmt.Println(s)
-
-	s = strconv.FormatInt(134020777, 16)
-	fmt.Println(s)
+	i, _ = strconv.Atoi("350")                 // 350
+	fmt.Sscanf("57", "%d", &i)                 // 57
+	k, _ = strconv.ParseInt("cc7fdd", 16, 32)  // 13402077
+	k, _ = strconv.ParseInt("0xcc7fdd", 0, 32) // 13402077
+	f, _ = strconv.ParseFloat("3.14", 64)      // 3.14
+	s = strconv.Itoa(340)                      // "340"
+	s = strconv.FormatInt(134020777, 16)       // "cc7fdd"
+	s = fmt.Sprint(3.14)                       // "3.14"
+	s = fmt.Sprintf("%x", 13402077)            // "cc7fdd"
 }
